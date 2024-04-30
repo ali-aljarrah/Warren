214a6c1da497f27b4926eba4649c91476043e0f4
ali-aljarrah <ali.jarrah.109@gmail.com>
2024-04-30 19:16:40 +0800

    first push

---
+++ index.php
@@ -0,0 +1,96 @@
+<?php include './include/head.php'; ?>
+    <title>TITLE</title>
+    <meta name="description" content="DESCRIPTION">
+    <link rel="canonical" href="https://www.URL.COM/">
+    <meta property="og:title" content="TITLE">
+    <meta property="og:description" content="DESCRIPTION">
+
+  </head>
+  <body>
+    <?php include './include/loader.php'; ?>
+    <?php include './include/menu.php'; ?>
+
+    <section class="py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mb-4 mb-lg-0">
+            <div class="mt-4">
+              <h1 class="dark-color-1 fs-48 fw-bold mb-4">
+              Experience the difference of <br/>truly personal care!
+              </h1>
+              <p class="black-color fs-16">
+              We are proud to serve our community and are committed to making a 
+              </br>positive impact on the oral health of our patients.
+              </p>
+            </div>
+            <div class="my-5">
+              <div class="d-flex align-items-lg-center flex-column flex-lg-row">
+                <div>
+                  <a href="#" class="btn-main-blue">Schedule appointment</a>
+                </div>
+                <div class="line-1 mx-4 d-none d-lg-block"></div>
+                <div class="mt-4 mt-lg-0">
+                  <div class="fs-16 gray-color">Emergency? Need to come ASAP?</div>
+                  <div class="fs-16 gray-color">
+                    <span class="dark-color-1 fs-16 fw-bold">Click here</span>
+                    <span>
+                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M5.47 5.47C5.61062 5.32955 5.80125 5.25066 6 5.25066C6.19875 5.25066 6.38937 5.32955 6.53 5.47L17.25 16.19V9C17.25 8.80109 17.329 8.61032 17.4697 8.46967C17.6103 8.32902 17.8011 8.25 18 8.25C18.1989 8.25 18.3897 8.32902 18.5303 8.46967C18.671 8.61032 18.75 8.80109 18.75 9V18C18.75 18.1989 18.671 18.3897 18.5303 18.5303C18.3897 18.671 18.1989 18.75 18 18.75H9C8.80108 18.75 8.61032 18.671 8.46967 18.5303C8.32901 18.3897 8.25 18.1989 8.25 18C8.25 17.8011 8.32901 17.6103 8.46967 17.4697C8.61032 17.329 8.80108 17.25 9 17.25H16.19L5.47 6.53C5.32955 6.38937 5.25066 6.19875 5.25066 6C5.25066 5.80125 5.32955 5.61063 5.47 5.47Z" fill="#121212"/>
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-8 mb-4 mb-lg-0">
+                <div class="h-100 w-100 bg-gray rounded-4 p-4">
+                  <h2 class="fs-24 fw-bold dark-color-1">We love your insurance.</h2>
+                  <p class="fs-16 dark-color-1">
+                  Most insured patients pay nothing out of pocket for services below:
+                  </p>
+                  <div class="mt-4">
+                    <div class="bg-gray-1 rounded-4 py-2 px-3 w-100 mb-2">
+                      <p class="fs-16 dark-color-1 mb-0">
+                        <span class="fw-bold">$0</span> - Brighter, Whiter Cleaning for Adults
+                      </p>
+                    </div>
+                    <div class="bg-gray-1 rounded-4 py-2 px-3 w-100 mb-2">
+                      <p class="fs-16 dark-color-1 mb-0">
+                        <span class="fw-bold">$0</span> - Cleaning & Exam for Children
+                      </p>
+                    </div>
+                    <div class="bg-gray-1 rounded-4 py-2 px-3 w-100 mb-2">
+                      <p class="fs-16 dark-color-1 mb-0">
+                        <span class="fw-bold">$0</span> - Exam for Adults
+                      </p>
+                    </div>
+                    <div class="bg-gray-1 rounded-4 py-2 px-3 w-100 mb-2">
+                      <p class="fs-16 dark-color-1 mb-0">
+                        <span class="fw-bold">Free</span> Second Opinion Evaluation
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="h-100 d-flex flex-column justify-content-end">
+                     <div class="bg-cyan rounded-4 p-4 text-end">
+                      <p class="fs-16 dark-color-1 fw-600 mb-0">
+                      Appointment available after work or school & on Saturdays.
+                      </p>
+                     </div> 
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 bg-colu-home">
+            <img class="img-fluid d-block d-lg-none" loading="lazy" width="673" height="788" src="/assets/imgs/home/img-1.webp" alt="WARREN - Experience the difference of truly personal care!">
+          </div>
+        </div>
+      </div>
+    </section> 
+
+    <?php include './include/footer.php'; ?>
+    </body>
+</html>
