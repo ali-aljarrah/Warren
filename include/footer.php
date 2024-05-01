45aeb9bb6bf0aaefbde7a3153bf5f983289fb513
ali-aljarrah <ali.jarrah.109@gmail.com>
2024-05-01 14:10:51 +0800

    Adding footer

--- include/footer.php
+++ include/footer.php
@@ -1,48 +1,93 @@
 <footer>
-    <div class="footer-top">
-        <div class="footer-card light">
-            <div class="text-center">
-                <a href="#" class="fs-24 fw-600 blue-color">
-                    <span class="me-2">Dental Implants</span>
-                    <span>
-                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 9C11.7348 9 11.4804 8.89464 11.2929 8.70711C11.1053 8.51957 11 8.26522 11 8C11 7.73478 11.1053 7.48043 11.2929 7.29289C11.4804 7.10536 11.7348 7 12 7H24C24.2652 7 24.5196 7.10536 24.7071 7.29289C24.8946 7.48043 25 7.73478 25 8V20C25 20.2652 24.8946 20.5196 24.7071 20.7071C24.5196 20.8946 24.2652 21 24 21C23.7348 21 23.4804 20.8946 23.2929 20.7071C23.1053 20.5196 23 20.2652 23 20V10.4133L8.70666 24.7067C8.51709 24.8833 8.26636 24.9795 8.00729 24.9749C7.74822 24.9703 7.50104 24.8654 7.31783 24.6822C7.13461 24.4989 7.02966 24.2518 7.02509 23.9927C7.02052 23.7336 7.11668 23.4829 7.29332 23.2933L21.5867 9H12Z" fill="#05395B"/>
-                        </svg>
-                    </span>
-                </a>
-                <p class="fs-16 blue-color mb-0 mt-4">
-                Eat, Chew & Smile Naturally Again! Dental implants look & feel like your own teeth. With a secure fit, you can finally eat what you want again!
-                </p>
+    <div class="container-fluid block-padding pt-5 pb-4">
+        <div class="row">
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <div class="mb-2">
+                    <a class="footer-link active" href="/">Home</a>
+                </div>
+                <div class="mb-2">
+                    <a class="footer-link" href="#">Patient Information</a>
+                </div>
+                <div class="mb-2">
+                    <a class="footer-link" href="#">Dental Services</a>
+                </div>
+                <div class="mb-2">
+                    <a class="footer-link" href="#">Contact</a>
+                </div>
+                <div>
+                    <a class="footer-link" href="#">Blog</a>
+                </div>
+            </div>
+            <div class="col-lg-2 mb-4 mb-lg-0">
+                <div class="fs-20 fw-bold dark-color-1 mb-2">Hours</div>
+                <div class="fs-16 dark-color-1 mb-1">
+                    <span class="fw-bold">Monday:</span> 9am-6pm
+                </div>
+                <div class="fs-16 dark-color-1 mb-1">
+                    <span class="fw-bold">Tuesday:</span> 9am-6pm
+                </div>
+                <div class="fs-16 dark-color-1 mb-1">
+                    <span class="fw-bold">Wednesday:</span> Closed
+                </div>
+                <div class="fs-16 dark-color-1 mb-1">
+                    <span class="fw-bold">Thursday:</span> 9am-6pm
+                </div>
+                <div class="fs-16 dark-color-1 mb-1">
+                    <span class="fw-bold">Friday:</span> 9am-4pm
+                </div>
+                <div class="fs-16 dark-color-1">
+                    <span class="fw-bold">Saturday:</span> 8am-2pm
+                </div>
+            </div>
+            <div class="offset-lg-1 col-lg-3 mb-4 mb-lg-0">
+                <div class="fs-20 fw-bold dark-color-1 mb-2">About us</div>
+                <div class="fs-16 dark-color-1">
+                In addition to our advanced technologies & innovative techniques, we've revolutionized dentistry to 
+                be truly personalized & convenient. No more 9-5! We have hours that fit your busy schedule, including evenings 
+                & Saturdays. Plus, we take the time to listen & clearly explain everything you need to know about your care.
+                </div>
+            </div>
+            <div class="col-lg-2 mb-4 mb-lg-0">
+                <div class="text-start text-lg-end">
+                    <div class="fs-20 fw-bold dark-color-1 mb-3">Address & Phone</div>
+                    <div class="fs-16 dark-color-1 mb-0">4224 E 10 Mile Rd,</div>
+                    <div class="fs-16 dark-color-1 mb-0">Warren, MI 48091,</div>
+                    <div class="fs-16 dark-color-1 mb-0">United States</div>
+                    <div class="my-4">
+                        <a href="tel:+586-756-6351" class="text-decoration-none fs-16 dark-color-1">586-756-6351</a>
+                    </div>
+                    <div>
+                    <a href="#" class="btn-main-blue ms-0 ms-lg-auto w-fit">Book a visit</a>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="footer-card dark">
-            <div class="text-center">
-                <a href="#" class="fs-24 fw-600 white-color">
-                    <span class="me-2">Invisalign® Clear Braces</span>
-                    <span>
-                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 9C11.7348 9 11.4804 8.89464 11.2929 8.70711C11.1053 8.51957 11 8.26522 11 8C11 7.73478 11.1053 7.48043 11.2929 7.29289C11.4804 7.10536 11.7348 7 12 7H24C24.2652 7 24.5196 7.10536 24.7071 7.29289C24.8946 7.48043 25 7.73478 25 8V20C25 20.2652 24.8946 20.5196 24.7071 20.7071C24.5196 20.8946 24.2652 21 24 21C23.7348 21 23.4804 20.8946 23.2929 20.7071C23.1053 20.5196 23 20.2652 23 20V10.4133L8.70666 24.7067C8.51709 24.8833 8.26636 24.9795 8.00729 24.9749C7.74822 24.9703 7.50104 24.8654 7.31783 24.6822C7.13461 24.4989 7.02966 24.2518 7.02509 23.9927C7.02052 23.7336 7.11668 23.4829 7.29332 23.2933L21.5867 9H12Z" fill="#ffffff"/>
-                        </svg>
-                    </span>
-                </a>
-                <p class="fs-16 white-color mb-0 mt-4">
-                Invisalign® is the revolutionary way to help you get even, straight teeth without noticeable, painful metal braces.
-                </p>
+        <hr class="my-5">
+        <div class="row">
+            <div class="col-lg-6 order-2 order-lg-1">
+                <div class="h-100 d-flex align-items-center">
+                    <div class="fs-16 gray-color-1">
+                    © <span id="current-date"></span> Warren Laser Dentistry All rights reserved 
+                    <a href="#" class="gray-color-1 text-decoration-underline">Privacy Policy</a> | 
+                    <a href="#" class="gray-color-1 text-decoration-underline">Terms and Conditions</a>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="footer-card light">
-            <div class="text-center">
-                <a href="#" class="fs-24 fw-600 blue-color">
-                    <span class="me-2">Reshape & Whiten Your Smile With Veneers</span>
-                    <span>
-                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 9C11.7348 9 11.4804 8.89464 11.2929 8.70711C11.1053 8.51957 11 8.26522 11 8C11 7.73478 11.1053 7.48043 11.2929 7.29289C11.4804 7.10536 11.7348 7 12 7H24C24.2652 7 24.5196 7.10536 24.7071 7.29289C24.8946 7.48043 25 7.73478 25 8V20C25 20.2652 24.8946 20.5196 24.7071 20.7071C24.5196 20.8946 24.2652 21 24 21C23.7348 21 23.4804 20.8946 23.2929 20.7071C23.1053 20.5196 23 20.2652 23 20V10.4133L8.70666 24.7067C8.51709 24.8833 8.26636 24.9795 8.00729 24.9749C7.74822 24.9703 7.50104 24.8654 7.31783 24.6822C7.13461 24.4989 7.02966 24.2518 7.02509 23.9927C7.02052 23.7336 7.11668 23.4829 7.29332 23.2933L21.5867 9H12Z" fill="#05395B"/>
+            <div class="col-lg-6 order-1 order-lg-2 mb-4 mb-lg-0">
+                <div class="text-start text-lg-end">
+                    <a class="me-2" href="#" target="_blank" rel="noopener noreferrer">
+                        <svg width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="62" height="62" rx="31" fill="white"/>
+                            <path d="M33.5 32.875H36.625L37.875 27.875H33.5V25.375C33.5 24.0875 33.5 22.875 36 22.875H37.875V18.675C37.4675 18.6213 35.9287 18.5 34.3037 18.5C30.91 18.5 28.5 20.5712 28.5 24.375V27.875H24.75V32.875H28.5V43.5H33.5V32.875Z" fill="#05395B"/>
+                        </svg> 
+                    </a>
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <svg width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="62" height="62" rx="31" fill="white"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M43.125 26.0462C43.008 25.158 42.6688 24.3136 42.1387 23.5912C41.4819 22.9295 40.5911 22.5528 39.6587 22.5425C36.2 22.2925 31.0013 22.2925 31.0013 22.2925C31.0013 22.2925 25.805 22.2925 22.3412 22.5425C21.4094 22.5532 20.5191 22.9298 19.8625 23.5912C19.3289 24.3122 18.9889 25.1578 18.875 26.0475C18.7202 27.3779 18.6367 28.7156 18.625 30.055V31.93C18.6367 33.2693 18.7202 34.6071 18.875 35.9375C18.9925 36.8275 19.33 37.675 19.855 38.4025C20.61 39.0725 21.58 39.4487 22.5887 39.4625C24.5675 39.6512 31.0025 39.7125 31.0025 39.7125C31.0025 39.7125 36.2038 39.7125 39.6625 39.4625C40.5954 39.4521 41.4868 39.0749 42.1438 38.4125C42.6712 37.6885 43.0094 36.8442 43.1275 35.9562C43.2823 34.6258 43.3658 33.2881 43.3775 31.9487V30.0537C43.3626 28.7144 43.2784 27.3768 43.125 26.0462ZM28.51 34.2387V27.2137L35.26 30.7375L28.51 34.2387Z" fill="#05395B"/>
                         </svg>
-                    </span>
-                </a>
-                <p class="fs-16 blue-color mb-0 mt-4">
-                Fix discolored, chipped or crooked teeth quickly & easily with natural-looking veneers.
-                </p>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
