7266f1ca9712cca981f8520e165668077b49a4f7
zainab98alrez <zainab.98ta@gmail.com>
2024-05-10 18:41:22 +0300

    create pages(meet us -contact-patient testimonials-privacy-term &conditions )

---
+++ contact.php
@@ -0,0 +1,270 @@
+<?php include './include/head.php'; ?>
+    <title>TITLE</title>
+    <meta name="description" content="DESCRIPTION">
+    <link rel="canonical" href="https://www.URL.COM/">
+    <meta property="og:title" content="TITLE">
+    <meta property="og:description" content="DESCRIPTION">
+
+  </head>
+  <body>
+    <?php include './include/loader.php'; ?>
+    <?php include './include/menu.php'; ?>
+
+    
+    <section class="py-5">
+      <div class="container-fluid block-padding">
+            <h1 class="dark-color-1 py-4 fs-40 fw-bold">
+                Contact
+            </h1>
+        <div class="row">
+            <div class="col-lg-6 mb-5 mb-lg-0">
+
+                 <p class="black-color fs-16 pt-5 mb-5 pe-5">
+                     If you are looking for dentists in Warren, MI, look no further than Warren Laser Dentistry for all of your dentistry needs.
+                 </p>
+                 <h5 class="dark-color-1 pt-5 pb-3 fs-24 fw-bold mt-5">
+                    A New Level of Respect for Your Family
+                 </h5>
+                 <p class="black-color fs-16">
+                    If you are looking for dentists in Warren, look no further than Warren Laser Dentistry for all of your dentistry needs. We are proud to have a highly educated & professional roster of dental experts that can help with any & all of your dental care needs & can do so right here in your own community to make it easy on you.
+                 </p>
+                 <p class="black-color fs-16 pt-2">
+                    In addition to our advanced technologies & innovative techniques, we’ve revolutionized dentistry to be truly personalized & convenient. We have hours that fit your busy schedule. Plus, we take the time to listen & clearly explain everything you need to know about your dental care.
+                 </p>
+            </div>
+            <div class="col-lg-5 offset-lg-1 mb-4 mb-lg-0">
+               <div class="row">
+                  <div class="col-4">
+                    <h5 class="dark-color-1 py-4 fs-24 fw-bold mt-4">
+                        Address
+                    </h5>
+                    <p class="black-color fs-16">
+                       4224 E 10 Mile Rd
+                    </p>
+                    <p class="black-color fs-16">
+                       Warren, MI 48091
+                    </p>
+                 </div>
+                 <div class="col-4">
+                    <h5 class="dark-color-1 py-4 fs-24 fw-bold mt-4">
+                       Phone
+                    </h5>
+                    <p class="black-color fs-16">
+                      586-756-6351
+                    </p>
+                 </div>
+                 <div class="col-4">
+                    <h5 class="dark-color-1 py-4 fs-24 fw-bold mt-4">
+                      Hours
+                    </h5>
+                    <p class="fs-16 dark-color-1 mb-0">
+                        <span class="fw-bold">Monday:</span>9am–6pm
+                      </p>
+                      <p class="fs-16 dark-color-1 mb-0">
+                        <span class="fw-bold">Tuesday:</span>9am–6pm
+                      </p>
+                      <p class="fs-16 dark-color-1 mb-0">
+                        <span class="fw-bold">Wednesday:</span> Closed
+                      </p>
+                      <p class="fs-16 dark-color-1 mb-0">
+                        <span class="fw-bold">Thursday:</span>9am–6pm
+                      </p>
+                      <p class="fs-16 dark-color-1 mb-0">
+                        <span class="fw-bold">Friday:</span>9am–4pm
+                      </p>
+                      <p class="fs-16 dark-color-1 mb-0">
+                        <span class="fw-bold">Saturday:</span>8am–2pm
+                      </p>
+                 </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <section class="py-5">
+      <div class="container-fluid p-0">
+         <div>
+                <img class="img-fluid" loading="lazy" width="1920" height="500" src="/assets/imgs/image 28.png" alt="WARREN - Experience the difference of truly personal care!">
+         </div>
+      </div>
+    </section>
+
+
+    <section class="py-5">
+      <div class="container-fluid block-padding">
+        <div class="row">
+            <div class="col-lg-6 mb-lg-0">
+                 <h5 class="dark-color-1 pt-2 pb-2 fs-24 fw-bold ">
+                     Map − Directions
+                 </h5>
+                 <p class="black-color fs-16">
+                    We are located near the corner of East 10 Mile & Ryan roads, across from Family Dollar. Please use the map below to find directions to our office. If you have any questions, please call us at  586-756-6351.
+                 </p>
+                 <p class="black-color fs-16 pt-2">
+                    In addition to our advanced technologies & innovative techniques, we’ve revolutionized dentistry to be truly personalized & convenient. We have hours that fit your busy schedule. Plus, we take the time to listen & clearly explain everything you need to know about your dental care.
+                 </p>
+                 <div class="row">
+                    <div class="col-lg-4">
+                      <p class="black-color fs-16 pt-2">
+                         Enter your starting address
+                      </p>   
+                    </div>
+                    <div class="col-lg-5 mb-3">
+                         <div class="pb-1">
+                           <input type="text" name="address" class="form-control custom-input bg-input" placeholder="Your starting address"/>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                    <div>
+                        <a href="#" class="btn-main-blue w-fit mx-auto">Get directions</a>
+                    </div>
+                    </div>
+                 </div>
+             </div>
+             <div class="col-lg-6 mb-5 mb-lg-0"></div>
+          </div>
+        </div> 
+    </section>
+
+
+
+
+
+
+    <section class="py-5">
+      <div class="container-fluid block-padding">
+        <div class="row">
+            <div class="col-lg-6 mb-lg-0">
+                 <h5 class="dark-color-1 pb-5 fs-24 fw-bold ">
+                     Appointments to Fit Your Schedule
+                 </h5>
+                 <p class="black-color fs-16">
+                     At Warren Laser Dentistry, we provide your family with the dental care you need without having to take time off from work or school for your dentist appointment. We understand that you, our valued patients, lead busy lives & it can be difficult to put aside an hour out of your busy day for a teeth cleaning. That is why we do all we can to open our schedule to allow you to come in when it doesn’t interfere with your day-to-day responsibilities—It’s just another way we show our patients how much we care.
+                 </p>
+                 <h5 class="dark-color-1 pt-5 pb-4 fs-24 fw-bold ">
+                     Saturday Appointments
+                 </h5>
+                 <p class="black-color fs-16">
+                     It seems like our patients are busier & busier lately. With all the running around that comes with modern life, it’s easy to let your obligation to your smile fall through the cracks. That’s why we’re committed to making dental care as convenient & accessible as possible by being a dentist that’s open on weekends.
+                 </p>
+                 <p class="black-color fs-16 pt-3">
+                    We believe that dentists that are open on Saturdays offer better access to preventive care, such as cleanings & exams. Better access to preventive care means fewer big dental problems in the future, which means less discomfort, pain & expense. We don’t want you to have to choose between work or school & keeping your smile healthy.
+                 </p>
+            
+             </div>
+             <div class="col-lg-5 offset-lg-1 mb-5 mb-lg-0">
+             <div class="fs-24 dark-color-1 fw-600 mb-4 ps-5">Get in touch</div>
+              <div class="rounded-4 ps-5 py-4 pe-4">
+                <form action="#" class="row">
+                  <div class="col-lg-6 mb-3">
+                    <div class="form-group">
+                      <input type="text" name="firstName" class="form-control custom-input bg-gray-2" placeholder="First name"/>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 mb-3">
+                    <div class="form-group">
+                      <input type="text" name="lastName" class="form-control custom-input bg-gray-2" placeholder="Last name"/>
+                    </div>
+                  </div>
+                  <div class="col-lg-12 mb-3">
+                  <div class="form-group">
+                      <input type="text" name="email" class="form-control custom-input bg-gray-2" placeholder="Your email address"/>
+                    </div>
+                  </div>
+                  <div class="col-lg-12 mb-3">
+                  <div class="form-group">
+                      <input type="phone" name="phone" class="form-control custom-input bg-gray-2" placeholder="Your phone number"/>
+                    </div>
+                  </div>
+                  <div class="col-lg-12 mb-3">
+                  <div class="form-group">
+                      <input type="textarea" name="textarea" class="form-control custom-input bg-gray-2" placeholder="Write your message here"/>
+                    </div>
+                  </div>
+                  <div class="col-lg-12 mt-4 text-start">
+                  <a href="#" class="btn btn-light rounded-5 bt-style">Submit message</a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div> 
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+              
+    <section>
+      <div class="footer-top">
+          <div class="footer-card light">
+              <div class="text-center">
+                  <a href="#" class="fs-24 fw-600 blue-color">
+                      <span class="me-2">Dental Implants</span>
+                      <span>
+                          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd" d="M12 9C11.7348 9 11.4804 8.89464 11.2929 8.70711C11.1053 8.51957 11 8.26522 11 8C11 7.73478 11.1053 7.48043 11.2929 7.29289C11.4804 7.10536 11.7348 7 12 7H24C24.2652 7 24.5196 7.10536 24.7071 7.29289C24.8946 7.48043 25 7.73478 25 8V20C25 20.2652 24.8946 20.5196 24.7071 20.7071C24.5196 20.8946 24.2652 21 24 21C23.7348 21 23.4804 20.8946 23.2929 20.7071C23.1053 20.5196 23 20.2652 23 20V10.4133L8.70666 24.7067C8.51709 24.8833 8.26636 24.9795 8.00729 24.9749C7.74822 24.9703 7.50104 24.8654 7.31783 24.6822C7.13461 24.4989 7.02966 24.2518 7.02509 23.9927C7.02052 23.7336 7.11668 23.4829 7.29332 23.2933L21.5867 9H12Z" fill="#05395B"/>
+                          </svg>
+                      </span>
+                  </a>
+                  <p class="fs-16 blue-color mb-0 mt-4">
+                  Eat, Chew & Smile Naturally Again! Dental implants look & feel like your own teeth. With a secure fit, you can finally eat what you want again!
+                  </p>
+              </div>
+          </div>
+          <div class="footer-card dark">
+              <div class="text-center">
+                  <a href="#" class="fs-24 fw-600 white-color">
+                      <span class="me-2">Invisalign® Clear Braces</span>
+                      <span>
+                          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd" d="M12 9C11.7348 9 11.4804 8.89464 11.2929 8.70711C11.1053 8.51957 11 8.26522 11 8C11 7.73478 11.1053 7.48043 11.2929 7.29289C11.4804 7.10536 11.7348 7 12 7H24C24.2652 7 24.5196 7.10536 24.7071 7.29289C24.8946 7.48043 25 7.73478 25 8V20C25 20.2652 24.8946 20.5196 24.7071 20.7071C24.5196 20.8946 24.2652 21 24 21C23.7348 21 23.4804 20.8946 23.2929 20.7071C23.1053 20.5196 23 20.2652 23 20V10.4133L8.70666 24.7067C8.51709 24.8833 8.26636 24.9795 8.00729 24.9749C7.74822 24.9703 7.50104 24.8654 7.31783 24.6822C7.13461 24.4989 7.02966 24.2518 7.02509 23.9927C7.02052 23.7336 7.11668 23.4829 7.29332 23.2933L21.5867 9H12Z" fill="#ffffff"/>
+                          </svg>
+                      </span>
+                  </a>
+                  <p class="fs-16 white-color mb-0 mt-4">
+                  Invisalign® is the revolutionary way to help you get even, straight teeth without noticeable, painful metal braces.
+                  </p>
+              </div>
+          </div>
+          <div class="footer-card light">
+              <div class="text-center">
+                  <a href="#" class="fs-24 fw-600 blue-color">
+                      <span class="me-2">Reshape & Whiten Your Smile With Veneers</span>
+                      <span>
+                          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd" d="M12 9C11.7348 9 11.4804 8.89464 11.2929 8.70711C11.1053 8.51957 11 8.26522 11 8C11 7.73478 11.1053 7.48043 11.2929 7.29289C11.4804 7.10536 11.7348 7 12 7H24C24.2652 7 24.5196 7.10536 24.7071 7.29289C24.8946 7.48043 25 7.73478 25 8V20C25 20.2652 24.8946 20.5196 24.7071 20.7071C24.5196 20.8946 24.2652 21 24 21C23.7348 21 23.4804 20.8946 23.2929 20.7071C23.1053 20.5196 23 20.2652 23 20V10.4133L8.70666 24.7067C8.51709 24.8833 8.26636 24.9795 8.00729 24.9749C7.74822 24.9703 7.50104 24.8654 7.31783 24.6822C7.13461 24.4989 7.02966 24.2518 7.02509 23.9927C7.02052 23.7336 7.11668 23.4829 7.29332 23.2933L21.5867 9H12Z" fill="#05395B"/>
+                          </svg>
+                      </span>
+                  </a>
+                  <p class="fs-16 blue-color mb-0 mt-4">
+                  Fix discolored, chipped or crooked teeth quickly & easily with natural-looking veneers.
+                  </p>
+              </div>
+          </div>
+      </div>
+    </section>
+
+    <?php include './include/footer.php'; ?>
+    </body>
+</html>
---
+++ meet-us.php
@@ -0,0 +1,114 @@
+<?php include './include/head.php'; ?>
+    <title>TITLE</title>
+    <meta name="description" content="DESCRIPTION">
+    <link rel="canonical" href="https://www.URL.COM/">
+    <meta property="og:title" content="TITLE">
+    <meta property="og:description" content="DESCRIPTION">
+
+  </head>
+  <body>
+    <?php include './include/loader.php'; ?>
+    <?php include './include/menu.php'; ?>
+
+    
+    <section class="py-5">
+      <div class="container-fluid block-padding">
+        <div class="row">
+            <div>
+                <img class="img-fluid" loading="lazy" width="1920" height="500" src="/assets/imgs/Union.png" alt="WARREN - Experience the difference of truly personal care!">
+            </div>
+            <div class="col-lg-7 mb-4 mb-lg-0 drop">
+                 <h1 class="dark-color-1 py-4 fs-40 fw-bold">
+                    Meet Us
+                 </h1>
+                 <p class="black-color fs-16">
+                     At Warren Laser Dentistry, we are a team of caring, experienced dental professionals who use only the most advanced technologies, materials & procedures. At our community-focused practice, your comfort & satisfaction come first. We offer early morning, late evening & Saturday appointments, so you don’t have to miss work or school for your care, something our team feels strongly about.
+                  </p>
+                 <p class="black-color fs-16">
+                     We pride ourselves on taking the time to listen & get to know each patient as an individual. The entire team at Warren Laser Dentistry looks forward to seeing you soon & developing a relationship with you to build the bridge toward long-term trust & successful dental care. See why our patients can’t stop smiling!
+                 </p>
+            </div>
+        </div>
+      </div>
+    </section>
+
+    
+    <section class="py-5">
+      <div class="container-fluid block-padding">
+        <div class="row">
+                 <h5 class="dark-color-1 py-4 fs-24 fw-bold">
+                     Warren Laser Dentistry Staff
+                 </h5>
+                 <p class="black-color fs-16">
+                    Our exceptional, friendly staff is our greatest asset. All of our dental assistants & hygienists are licensed professionals who pride themselves on their current dental techniques & outstanding patient communication. With unmatched quality & integrity, they are dedicated to achieving the best possible results for you & your family.
+                 </p>
+                 <p class="black-color fs-16">
+                     Since your comfort & health are our top priorities, Warren Laser Dentistry’s staff is glad to assist you in any way. If you have any questions at all, from treatment options & sterilization to insurance concerns & paperwork, we’re here for you! We can assure you that you’ll enjoy your dental visit!
+                 </p>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+
+
+
+
+    
+              
+    <section>
+      <div class="footer-top">
+          <div class="footer-card light">
+              <div class="text-center">
+                  <a href="#" class="fs-24 fw-600 blue-color">
+                      <span class="me-2">Dental Implants</span>
+                      <span>
+                          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd" d="M12 9C11.7348 9 11.4804 8.89464 11.2929 8.70711C11.1053 8.51957 11 8.26522 11 8C11 7.73478 11.1053 7.48043 11.2929 7.29289C11.4804 7.10536 11.7348 7 12 7H24C24.2652 7 24.5196 7.10536 24.7071 7.29289C24.8946 7.48043 25 7.73478 25 8V20C25 20.2652 24.8946 20.5196 24.7071 20.7071C24.5196 20.8946 24.2652 21 24 21C23.7348 21 23.4804 20.8946 23.2929 20.7071C23.1053 20.5196 23 20.2652 23 20V10.4133L8.70666 24.7067C8.51709 24.8833 8.26636 24.9795 8.00729 24.9749C7.74822 24.9703 7.50104 24.8654 7.31783 24.6822C7.13461 24.4989 7.02966 24.2518 7.02509 23.9927C7.02052 23.7336 7.11668 23.4829 7.29332 23.2933L21.5867 9H12Z" fill="#05395B"/>
+                          </svg>
+                      </span>
+                  </a>
+                  <p class="fs-16 blue-color mb-0 mt-4">
+                  Eat, Chew & Smile Naturally Again! Dental implants look & feel like your own teeth. With a secure fit, you can finally eat what you want again!
+                  </p>
+              </div>
+          </div>
+          <div class="footer-card dark">
+              <div class="text-center">
+                  <a href="#" class="fs-24 fw-600 white-color">
+                      <span class="me-2">Invisalign® Clear Braces</span>
+                      <span>
+                          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd" d="M12 9C11.7348 9 11.4804 8.89464 11.2929 8.70711C11.1053 8.51957 11 8.26522 11 8C11 7.73478 11.1053 7.48043 11.2929 7.29289C11.4804 7.10536 11.7348 7 12 7H24C24.2652 7 24.5196 7.10536 24.7071 7.29289C24.8946 7.48043 25 7.73478 25 8V20C25 20.2652 24.8946 20.5196 24.7071 20.7071C24.5196 20.8946 24.2652 21 24 21C23.7348 21 23.4804 20.8946 23.2929 20.7071C23.1053 20.5196 23 20.2652 23 20V10.4133L8.70666 24.7067C8.51709 24.8833 8.26636 24.9795 8.00729 24.9749C7.74822 24.9703 7.50104 24.8654 7.31783 24.6822C7.13461 24.4989 7.02966 24.2518 7.02509 23.9927C7.02052 23.7336 7.11668 23.4829 7.29332 23.2933L21.5867 9H12Z" fill="#ffffff"/>
+                          </svg>
+                      </span>
+                  </a>
+                  <p class="fs-16 white-color mb-0 mt-4">
+                  Invisalign® is the revolutionary way to help you get even, straight teeth without noticeable, painful metal braces.
+                  </p>
+              </div>
+          </div>
+          <div class="footer-card light">
+              <div class="text-center">
+                  <a href="#" class="fs-24 fw-600 blue-color">
+                      <span class="me-2">Reshape & Whiten Your Smile With Veneers</span>
+                      <span>
+                          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd" d="M12 9C11.7348 9 11.4804 8.89464 11.2929 8.70711C11.1053 8.51957 11 8.26522 11 8C11 7.73478 11.1053 7.48043 11.2929 7.29289C11.4804 7.10536 11.7348 7 12 7H24C24.2652 7 24.5196 7.10536 24.7071 7.29289C24.8946 7.48043 25 7.73478 25 8V20C25 20.2652 24.8946 20.5196 24.7071 20.7071C24.5196 20.8946 24.2652 21 24 21C23.7348 21 23.4804 20.8946 23.2929 20.7071C23.1053 20.5196 23 20.2652 23 20V10.4133L8.70666 24.7067C8.51709 24.8833 8.26636 24.9795 8.00729 24.9749C7.74822 24.9703 7.50104 24.8654 7.31783 24.6822C7.13461 24.4989 7.02966 24.2518 7.02509 23.9927C7.02052 23.7336 7.11668 23.4829 7.29332 23.2933L21.5867 9H12Z" fill="#05395B"/>
+                          </svg>
+                      </span>
+                  </a>
+                  <p class="fs-16 blue-color mb-0 mt-4">
+                  Fix discolored, chipped or crooked teeth quickly & easily with natural-looking veneers.
+                  </p>
+              </div>
+          </div>
+      </div>
+    </section>
+
+    <?php include './include/footer.php'; ?>
+    </body>
+</html>
