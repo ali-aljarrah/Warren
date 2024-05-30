1ca7885bde9cf299b306dfff2fb0a3d065f4045d
ali-aljarrah <ali.jarrah.109@gmail.com>
2024-05-30 19:44:39 +0800

    Add active state to some pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,52 +7,17 @@ document.addEventListener("DOMContentLoaded", () => {
     //     loader.classList.remove("show");
     // }, 1000);
 
-    // let url = location.pathname;
-    // const navLinkClasses = document.querySelectorAll('.nav-link');
+    let url = location.pathname;
 
-    // if(url === '/about') {
-    //     removeActiveFromMenuLinks();
-    //     document.getElementById('about-link').classList.add('active');
-    //     document.getElementById('sm-about-link').classList.add('active');
-    // } else if(url === '/dental-and-medical-it') {
-    //     removeActiveFromMenuLinks();
-    //     activeServicesMenuLinks();
-    //     document.getElementById('sm-medical-link').classList.add('active');
-    //     document.getElementById('medical-link').classList.add('active');
-    // } else if(url === '/web-development') {
-    //     removeActiveFromMenuLinks();
-    //     activeServicesMenuLinks();
-    //     document.getElementById('sm-web-link').classList.add('active');
-    //     document.getElementById('web-link').classList.add('active');
-    // } else if(url === '/machine-learning') {
-    //     removeActiveFromMenuLinks();
-    //     activeServicesMenuLinks();
-    //     document.getElementById('sm-machine-link').classList.add('active');
-    //     document.getElementById('machine-link').classList.add('active');
-    // } else if(url === '/digital-marketing') {
-    //     removeActiveFromMenuLinks();
-    //     activeServicesMenuLinks();
-    //     document.getElementById('sm-digital-link').classList.add('active');
-    //     document.getElementById('digital-link').classList.add('active');
-    // } else if(url === '/smart-home') {
-    //     removeActiveFromMenuLinks();
-    //     activeServicesMenuLinks();
-    //     document.getElementById('sm-smart-link').classList.add('active');
-    //     document.getElementById('smart-link').classList.add('active');
-    // } else if(url === '/contact-us') {
-    //     removeActiveFromMenuLinks();
-    //     document.getElementById('contact-link').classList.add('active');
-    //     document.getElementById('sm-contact-link').classList.add('active');
-    // } else if(url === '/assistance-hub') {
-    //     removeActiveFromMenuLinks();
-    //     document.getElementById('assistance-link').classList.add('active');
-    //     document.getElementById('sm-assistance-link').classList.add('active');
-    // } else if(url === '/emr-development') {
-    //     removeActiveFromMenuLinks();
-    //     activeServicesMenuLinks();
-    //     document.getElementById('emr-link').classList.add('active');
-    //     document.getElementById('sm-emr-link').classList.add('active');
-    // }
+    if(url === '/patient-information') {
+        document.getElementById("patient-information-lg-dropdown-link").classList.add('active');
+        document.getElementById("patient-information-lg-link").classList.add('active');
+    } else if (url === '/scheduling') {
+        document.getElementById("patient-information-lg-dropdown-link").classList.add('active');
+        document.getElementById("scheduling-lg-link").classList.add('active');
+    }
+
+ 
 
     const smMenuBtn = document.getElementById("sm-navbar-btn");
     const burgerIcon = document.getElementById("burger-icon");
@@ -85,30 +50,5 @@ document.addEventListener("DOMContentLoaded", () => {
         } 
     }
 
-    function removeActiveFromMenuLinks() {
-        navLinkClasses.forEach(el => {
-            el.classList.remove('active');
-        });
-    }
-
-    function activeServicesMenuLinks() {
-        document.getElementById('services-dropdown-link').classList.add('active');
-        document.getElementById('services-accordion-sm-btn').classList.remove('collapsed');
-        document.getElementById('services-accordion-sm-btn').classList.add('active');
-        document.getElementById('collapseServices').classList.add('show');
-    }
-
-
-    // const textAnchors = document.querySelectorAll('.text-anchor');
-
-    // setInterval(() => {
-    //     textAnchors.forEach(el => {
-    //         if(el.style.opacity == '0') {
-    //             el.style.opacity = '1';
-    //         } else {
-    //             el.style.opacity = '0';
-    //         }
-    //     });
-    // }, 350);
 });
 
